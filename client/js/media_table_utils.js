7e5f4c47d232f352b4d7b15ad6c4faa66c63fdc9
Linoy Ben David <linoybd@liacom.co.il>
2022-05-12 16:22:15 +0300

    add update_media_function

--- client/js/media_table_utils.js
+++ client/js/media_table_utils.js
@@ -1,12 +1,16 @@
 let mediaData;
+let curr_update_media_id;
 
 $(document).ready(function () {
 
     $("#addMedia_btn_1").click(openAddMedia);
     $("#addMedia_btn_2").click(openAddMedia);
     $("#close_btn").click(closeAddMedia);
+    $("#update_close_btn").click(closeUpdateMedia);
+
 
     closeAddMedia();
+    closeUpdateMedia();
     fillTable();
     
      // Show fields when series checkbox is on
@@ -53,6 +57,16 @@ $(document).ready(function () {
             // stop the form from submitting the normal way and refreshing the page
             event.preventDefault();
         });
+
+        $('#update_media_form').submit(function (event) {
+            submitUpdateMedia();
+            // stop the form from submitting the normal way and refreshing the page
+            event.preventDefault();
+        });
+
+
+        
+
     //$("#add_btn").click(submitAddMedia);
 
 
@@ -92,10 +106,10 @@ function fillTable()
                         '<td>' + '<img src = "' + object["picture"] + '"/img>' + '</td>' +
                         '<td>' + object["rating"] + '</td>' +
                         '<td>' + object["date"] + '</td>'+
-                        '<td>'+ "<button id = \"" + object["id?"] + "_updateMedia" + "\" onclick = updateMedia("+object["id?"]+") > update </button>" +
-                        "<br>" + "<button id = \"" + object["id?"] + "_addActor" + "\" onclick = addActor("+object["id?"]+") > add actor </button>" +
-                        "<br>" + "<button id = \"" + object["id?"] + "_viewActors" + "\" onclick = viewActors("+object["id?"]+") > view actors </button>" +
-                        "<br>" + "<button id = \"" + object["id?"] + "\" onclick = removeMedia("+object["id?"]+") > remove media </button>" + "</td>";
+                        '<td>'+ "<button id = \"" + object["id?"] + "_updateMedia" + "\" onclick = openUpdateMedia(\""+object["id?"]+"\") > update </button>" +
+                        "<br>" + "<button id = \"" + object["id?"] + "_addActor" + "\" onclick = addActor(\""+object["id?"]+"\") > add actor </button>" +
+                        "<br>" + "<button id = \"" + object["id?"] + "_viewActors" + "\" onclick = viewActors(\""+object["id?"]+"\") > view actors </button>" +
+                        "<br>" + "<button id = \"" + object["id?"] + "\" onclick = removeMedia(\""+object["id?"]+"\") > remove media </button>" + "</td>";
 
         table.appendChild(tr);
         //getViewsJSON(object,tr);
@@ -150,10 +164,66 @@ function submitAddMedia(){
     closeAddMedia();
 }
 
-function updateMedia(media_id)
-{
+function openUpdateMedia(media_id){
 
+    curr_update_media_id = media_id;
+    let element = document.getElementById("div_update_media_form")
+    element.style.display = "block";
+
+    let media = mediaData.find(x => x[Object.keys(x)[0]] === media_id);
+
+    $("#update_name_field").val(media.name);
+    $("#update_pic_url_field").val(media.picture);
+    $("#update_director_field").val(media.director);
+    $("#update_date_field").val(media.date);
+    $("#update_rating_field").val(media.rating);
+    $("#update_is_series_field").val(media.isSeries);
+    $("#update_episodes_field").val(media.series_details);
 }
+
+function closeUpdateMedia(){
+
+    let element = document.getElementById("div_update_media_form")
+    element.style.display = "none";
+}
+
+function submitUpdateMedia(){
+    //if(!$("#user_form").valid()) return;
+
+    // process the form
+    $.ajax({
+        type: 'PUT', 
+        url: '/movie/'+curr_update_media_id, 
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "movie_id": curr_update_media_id,
+            "movieDetails":{
+            "movieId": curr_update_media_id,
+            "name": $("#update_name_field").val(),
+            "picture": $("#update_pic_url_field").val(),
+            "director": $("#update_director_field").val(),
+            "date": $("#update_date_field").val(),
+            "rating": $("#update_rating_field").val(),
+            "isSeries": $("#update_is_series_field").val(),
+            "series_details" : "["+$("#update_episodes_field").val()+"]",
+            }}),
+        processData: false,            
+        encode: true,
+        success: function( data, textStatus, jQxhr ){
+            //console.log(data);
+            alert("media update!");
+            fillTable();
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+            alert("media not update!");
+        }
+    })
+
+    closeUpdateMedia();
+}
+
+
 
 function addActor(media_id)
 {
